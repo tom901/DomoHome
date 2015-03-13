99f0d68b19cd99fdcc6e0080caa2db05ae795dc4
tom9010 <thomas.paraiso@me.com>
2015-03-13 14:43:47 +0100

    Creation of the first Floor with 5 rooms and hard coded parameters

--- src/app/data/home/Room.java
+++ src/app/data/home/Room.java
@@ -1,6 +1,9 @@
 package app.data.home;
 
 import app.data.object.ObjectHome;
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 import java.util.ArrayList;
 
@@ -14,12 +17,35 @@ public class Room {
     public ArrayList<ObjectHome> objectHomes;
     double temperature;
 
+    //Atrtributes private
+
+
     public Room(){
         objectHomes = new ArrayList<ObjectHome>();
         Id = 0 ;
         temperature = 0.0;
     }
+    public static Group getRoom(double width, double height, int x, int y){
+        Group group = new Group();
+        Rectangle room = new Rectangle(width,height,Color.WHITE);
+        room.setX(x);
+        room.setY(y);
+        room.setStroke(Color.BLACK);
+        room.setStrokeWidth(1);
+        group.getChildren().add(room);
+        return group;
+    }
 
+    public static Group getGarden(double width, double height, int x, int y){
+        Group group = new Group();
+        Rectangle room = new Rectangle(width,height,Color.GREEN);
+        room.setX(x);
+        room.setY(y);
+        room.setStroke(Color.BLACK);
+        room.setStrokeWidth(1);
+        group.getChildren().add(room);
+        return group;
+    }
     //Function to get Id room
     public void getIdRoom(){
 
