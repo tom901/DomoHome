6c6da2c756f990d2008a77f15329a27512bd197c
Nico Didier <ireality@free.fr>
2015-03-26 16:03:34 +0100

    Added Licence GPL text box.

--- src/app/data/home/Floor.java
+++ src/app/data/home/Floor.java
@@ -1,3 +1,13 @@
+/* ******************************************************
+ * Project alpha - Composants logiciels 2015.
+ * Copyright (C) 2015 <Binh-Minh.Bui-Xuan@ens-lyon.org>.
+ * GPL version>=3 <http://www.gnu.org/licenses/>.
+ * $Id: engine/Engine.java 2015-03-11 buixuan.
+ * Modified 2015/03/26 by :
+ * - nicolas.didier.fd@gmail.com
+ * - leslie.cha@hotmail.fr
+ * - thomas.paraison@me.com
+ * ******************************************************/
 package app.data.home;
 
 import app.data.Dimension;
